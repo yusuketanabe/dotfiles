0e8d38834d09dcb2305833bf0db3b1d66ee139ea
yusuketanabe <inalow@me.com>
2019-04-15 13:56:08 +0900

    add goenv PATH

--- fish/config.fish
+++ fish/config.fish
@@ -9,7 +9,8 @@ set -x PATH $HOME/.nodebrew/current/bin $PATH
 
 ## pyenv
 set -x PATH $HOME/.pyenv/bin $PATH
-eval (pyenv init - | source)
+# eval (pyenv init - | source) fishでは下の書き方。$pyenv initしたら下のように書いてくれと言われる。
+status --is-interactive; and source (pyenv init -|psub)
 
 ## SDKMAN -> /functions/sdk.fishに記載
 # パスが通っていないので、SDKコマンドを一回でも実行しないとgradleコマンド使えないのでhelpコマンド実行すると。
@@ -20,3 +21,16 @@ set fish_theme eden
 
 ## ls color (man lsで確認して)
 export LSCOLORS=hxfxcxdxbxegedabagacad
+
+## gcc/g++
+set -x PATH ~/usr/local/bin $PATH
+
+## goenv
+set -x PATH $HOME/.goenv/bin $PATH
+# eval (goenv init - | source) fishでは下の書き方。$goenv initしたら下のように書いてくれと言われる。
+status --is-interactive; and source (goenv init -|psub)
+## go
+# $go get -v github.com/go-delve/delve/cmd/dlv Delve Debuggerをゲットする。
+# パッケージを保管するところ。
+set -x GOPATH $HOME/go
+set -x PATH $GOPATH/bin $PATH
