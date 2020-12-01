2de882d1e2246477404e2a7f3408c926be58efcb
yusuketanabe <inalow@me.com>
2020-12-01 23:57:22 +0900

    settings added 'conda activate <SHELL_NAME>

--- fish/README.md
+++ fish/README.md
@@ -2,3 +2,11 @@
 * fzf
 * z
 * bass
+
+## Fish shellでsdkmanを動かす
+* ~/.config/fish/functions/sdk.fish を作成
+* これだけではshellを起動した時にPATHが通っていないとなり実行できない
+* config.fish に"sdk help > /dev/null"と記載する
+* ここまでで使えるようになるが、アップデートがあるとアップデートの待ちの状態で止まってしまう
+* ~/.sdkman/etc/config の"sdkman_auto_selfupdate"をtrueにする
+デフォルトではfalse
--- fish/config.fish
+++ fish/config.fish
@@ -5,12 +5,18 @@ set -x PATH $HOME/.cargo/bin $PATH
 set -x PATH $HOME/Exercism/bin $PATH
 
 ## nodebrew
-set -x PATH $HOME/.nodebrew/current/bin $PATH
+#set -x PATH $HOME/.nodebrew/current/bin $PATH
+
+## rbenv
+set -x PATH $HOME/.rbenv/bin $PATH
+status --is-interactive; and source (rbenv init -|psub)
 
 ## pyenv
 set -x PATH $HOME/.pyenv/bin $PATH
 # eval (pyenv init - | source) fishでは下の書き方。$pyenv initしたら下のように書いてくれと言われる。
 status --is-interactive; and source (pyenv init -|psub)
+## conda activate <SHELL_NAME>
+source (conda info --root)/etc/fish/conf.d/conda.fish
 
 ## SDKMAN -> /functions/sdk.fishに記載
 # パスが通っていないので、SDKコマンドを一回でも実行しないとgradleコマンド使えないのでhelpコマンド実行すると。
@@ -36,4 +42,4 @@ set -x PATH $GOPATH/bin $PATH
 
 ## Flutter
 # other Shell export --PATH="$PATH:`pwd`/flutter/bin"--
-set -x PATH $HOME/flutter/bin $PATH
+set -x PATH $HOME/WorkSpace/flutter/bin $PATH
